946bdd123c2ed964a9f7d577043b21c825bffec4
devAon <yewon9742@gmail.com>
2020-05-20 19:30:35 +0900

    jwtAthenticationEntryPoint 추가

--- jwt/src/main/java/com/devaon/jwt/config/SecurityConfigurer.java
+++ jwt/src/main/java/com/devaon/jwt/config/SecurityConfigurer.java
@@ -4,8 +4,10 @@ import com.devaon.jwt.filters.JwtRequestFilter;
 import com.devaon.jwt.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,8 +23,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  * Github : http://github.com/devAon
  */
 
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
     @Autowired
     private MyUserDetailsService myUserDetailsService;
 
@@ -52,7 +60,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable()
                 .authorizeRequests().antMatchers("/authenticate").permitAll().
                 anyRequest().authenticated().and().
-                exceptionHandling().and().sessionManagement()
+                exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
--- jwt/src/main/java/com/devaon/jwt/filters/JwtRequestFilter.java
+++ jwt/src/main/java/com/devaon/jwt/filters/JwtRequestFilter.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.security.web.http.SecurityHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
